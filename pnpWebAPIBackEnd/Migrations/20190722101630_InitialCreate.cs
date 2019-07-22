1ed257458dfb283664ae213727c3066b082e1c16
Nhlahla_Mike_Mabasa <nhlahlamikemabasa@gmail.com>
2019-07-22 17:14:20 +0200

    Created Migrations and Models and Users Controller

---
+++ pnpWebAPIBackEnd/Migrations/20190722101630_InitialCreate.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace pnpWebAPIBackEnd.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UsersDetails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Email = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    Title = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    FirstName = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    Surname = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    Nationality = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    Identification = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    DateofBirth = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    Gender = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    HomeNumber = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    WorkNumber = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    AddressType = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    StreetAddress = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    Suburb = table.Column<string>(nullable: true),
+                    City = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    PostalCode = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    Province = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    CommsMethod = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    UserType = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    LoginStatus = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    Active = table.Column<string>(type: "nvarchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsersDetails", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsersDetails_Email",
+                table: "UsersDetails",
+                column: "Email",
+                unique: true,
+                filter: "[Email] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UsersDetails");
+        }
+    }
+}
---
+++ pnpWebAPIBackEnd/Models/Users.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pnpWebAPIBackEnd.Models
+{
+    public class Users
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Email { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Title { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string FirstName { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Surname { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Nationality { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Identification { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string DateofBirth { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Gender { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string PhoneNumber { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string HomeNumber { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string WorkNumber { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string AddressType { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string StreetAddress { get; set; }
+        public string Suburb { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string City { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string PostalCode { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Province { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string CommsMethod { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string UserType { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Password { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string LoginStatus { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string Active { get; set; }
+    }
+}
